07f2e89696b867ae596c843d9c31f7a53e862c2d
DoHoangAnh <DoHoangAnh@HOANGANHDO>
2023-09-22 10:12:13 +0700

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+namespace Test
+{
+    public delegate int MyDelegate(int a, int b);
+
+    internal class Program
+    {
+        static void DemoMethodDelegateParameter(MyDelegate my, int x, int y, string msg)
+        {
+            var result = my(x, y);
+            Console.WriteLine(msg + result);
+        }
+        static void Main(string[] args)
+        {
+            DemoMethodDelegateParameter(Demo.Sum, 5, 6, "Hello ");
+        }
+
+    }
+    class Demo
+    {
+        public static int Sum(int a, int b) => (a + b);
+        public static int Sub(int a, int b) => (a - b);
+    }
+}
